0c02c5e74128fd26440afd2a771c8d916c03d0f2
PiaoDongHao <lcsc294459608@gmail.com>
2022-09-04 17:23:11 +0800

    create select activity and camera activity

---
+++ app/src/main/java/org/pytorch/helloworld/SelectActivity.java
@@ -0,0 +1,40 @@
+package org.pytorch.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+
+public class SelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+
+        //initialize widgets
+        Button btn_default = findViewById(R.id.btn_default);
+        Button btn_camera = findViewById(R.id.btn_camera);
+
+        //Bind onClick function
+        btn_default.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
