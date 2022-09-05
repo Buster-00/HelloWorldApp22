8e8c3f4a38ab05e738e5ec9879fc20e85a9ad4fb
Buster-00 <lcsc294459608@gmail.com>
2022-09-05 10:32:12 +0800

    implement the cameraActivity

--- app/src/main/java/org/pytorch/helloworld/CameraActivity.java
+++ app/src/main/java/org/pytorch/helloworld/CameraActivity.java
@@ -2,13 +2,105 @@ package org.pytorch.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.camerax.lib.CameraXActivity;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class CameraActivity extends AppCompatActivity {
+
+    ImageView img_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        //Initialize widget
+        img_view = findViewById(R.id.img_view);
+
+        //start camera activity
+        startActivityForResult(new Intent(CameraActivity.this, CameraXActivity.class), 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && data != null) {
+            //返回拍照的图片地址
+            Uri imageUri = data.getData();
+
+            //turn into the bitmap
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                int degree = getBitmapDegree(imageUri.getPath());
+                bitmap = rotateBitmapByDegree(bitmap,degree);
+                img_view.setImageBitmap(bitmap);
+                Log.i("rotation degree", String.valueOf(getBitmapDegree(imageUri.getPath())));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected int getBitmapDegree(String path)
+    {
+        int degree = 0;
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation)
+            {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return degree;
+    }
+
+    //rotate the bitmap
+    public static Bitmap rotateBitmapByDegree(Bitmap bm, int degree) {
+        Bitmap returnBm = null;
+
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+
+        returnBm = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), matrix, true);
+
+        if (returnBm == null) {
+            returnBm = bm;
+        }
+
+        if(bm != returnBm)
+        {
+            bm.recycle();
+        }
+        return returnBm;
+
     }
 }
--- app/src/main/java/org/pytorch/helloworld/SelectActivity.java
+++ app/src/main/java/org/pytorch/helloworld/SelectActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.material.button.MaterialButton;
+
 
 public class SelectActivity extends AppCompatActivity {
 
