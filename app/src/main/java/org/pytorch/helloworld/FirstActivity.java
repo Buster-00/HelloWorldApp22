f24810c59a96080c26f93d13251786ca862cf481
Buster-00 <lcsc294459608@gmail.com>
2022-09-19 09:33:38 +0800

    update

---
+++ app/src/main/java/org/pytorch/helloworld/FirstActivity.java
@@ -0,0 +1,70 @@
+package org.pytorch.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class FirstActivity extends AppCompatActivity {
+
+    String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+
+        //set status bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        //request permission
+        requestPermission();
+
+        //initialize widgets
+        FloatingActionButton btn_default = findViewById(R.id.btn_default);
+        FloatingActionButton btn_camera = findViewById(R.id.btn_camera);
+        FloatingActionButton btn_debug = findViewById(R.id.btn_debug);
+
+        //Bind onClick function
+        btn_default.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstActivity.this, CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_debug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, debugActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void requestPermission()
+    {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, permissions,100);
+        }
+    }
+}
