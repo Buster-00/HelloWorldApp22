fc8a9632db0ba855e0678e1e3a57921f836f87ec
Buster-00 <lcsc294459608@gmail.com>
2022-09-21 18:16:38 +0800

    update

--- app/src/main/java/org/pytorch/helloworld/Camera02Activity.java
+++ app/src/main/java/org/pytorch/helloworld/Camera02Activity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Camera02Activity extends AppCompatActivity {
+public class Camera02Activity extends CameraActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera02);
+        setContentView(R.layout.activity_camera);
+
+
     }
 }
