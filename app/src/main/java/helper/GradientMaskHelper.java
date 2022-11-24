381b9f4c1fca8bbe01ad7946ca4eb04808ff1943
PiaoDongHao <lcsc294459608@gmail.com>
2022-11-24 12:08:38 +0800

    Update GradientMaskHelper.java

--- app/src/main/java/helper/GradientMaskHelper.java
+++ app/src/main/java/helper/GradientMaskHelper.java
@@ -14,25 +14,12 @@ import org.opencv.core.Scalar;
 
 public class GradientMaskHelper {
 
-    private static int STRENGTH = 500;
+    private static int STRENGTH = 800;
 
     //增加图像模糊遮罩
     private static Mat add_mask_xy(Mat img, int centerX, int centerY, int strength, int radius){
         int rows = img.rows();
         int cols = img.cols();
-
-        //获取图像数据
-//        int numChannels = img.channels();
-//        int frameSize = img.rows() * img.cols();
-//        byte[] buffer = new byte[numChannels*frameSize];
-//        img.get(0,0, buffer);
-
-//        byte[][] out=new byte[frameSize][numChannels];
-//        for (int p=0,i = 0; p < frameSize; p++) {
-//            for (int n = 0; n < numChannels; n++,i++) {
-//                out[p][n]= (byte) (buffer[i]&0xff);
-//            }
-//        }
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
@@ -48,10 +35,8 @@ public class GradientMaskHelper {
                 if(distance < radius * radius){
                     //按照距离大小计算增强的遮罩值
                     int val = (int)(strength * (1.0 - (sqrt(distance) / radius)));
-//                    out[i * cols + j][0] += result;
-//                    out[i * cols + j][1] += result;
-//                    out[i * cols + j][2] += result;
 
+                    //获取BGR数据，将其转为int
                     int B = (int) currentPixelData[0];
                     int G = (int) currentPixelData[1];
                     int R = (int) currentPixelData[2];
@@ -68,18 +53,10 @@ public class GradientMaskHelper {
             }
         }
 
-//        for (int p=0,i = 0; p < frameSize; p++) {
-//            for (int n = 0; n < numChannels; n++,i++) {
-//                buffer[i] = out[p][n];
-//            }
-//        }
-//        img.put(0,0,buffer);
-
         return img;
     }
 
     public static Mat GradientMaskMaker(Mat img1, Mat img2, int CenterX, int CenterY){
-
         //加上遮罩
         return add_mask_xy(img1, CenterX, CenterY, 200, 50);
     }
@@ -89,15 +66,12 @@ public class GradientMaskHelper {
         imgRE1.copyTo(imgRE1_);
 
         //生成一个渐变mask
-        Mat mat = new Mat(rows, cols, CvType.CV_8UC3, new Scalar(0,0,0));
-        //Mat mask = add_mask_xy(mat, rows/2, cols/2, 200, 50);
-        Mat mask = add_mask_xy(mat, CenterX, CenterY, STRENGTH, radius);
+        Mat temp = new Mat(rows, cols, CvType.CV_8UC3, new Scalar(0,0,0));
+        Mat mask = add_mask_xy(temp, CenterX, CenterY, STRENGTH, radius);
 
-        //Point center = new Point(rows/2, cols/2);
         Point center = new Point(CenterX, CenterY);
         Mat result = new Mat(imgRE1.rows(), imgRE1.cols(), CvType.CV_8UC3);
         seamlessClone(imgRE2, imgRE1, mask, center, result, MIXED_CLONE);
-        //imgRE2.copyTo(imgRE1_, mask);
         return result;
     }
 }
